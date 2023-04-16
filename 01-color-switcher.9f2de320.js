93bb19c87d92bb1bd32ddbc59c25616ad93aeb93
SerhiiFrolikov <116305785+SerhiiFrolikov@users.noreply.github.com>
2023-04-16 22:45:23 +0000

    Deploying to gh-pages from @ SerhiiFrolikov/goit-js-hw-09@f94c75fc2aed614daf2089937056175ad4b10641 🚀

---
+++ 01-color-switcher.9f2de320.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("body"),e=document.querySelector("button[data-start]"),n=document.querySelector("button[data-stop]");e.addEventListener("click",(function(){changeBgc=setInterval((function(){e.disabled=!0;var n="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0));t.style.backgroundColor=n}),1e3)})),n.addEventListener("click",(function(){clearInterval(changeBgc),e.disabled=!1}))}();
+//# sourceMappingURL=01-color-switcher.9f2de320.js.map
