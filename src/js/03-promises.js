3352310277f6817cdcfbc04351c92a00839be017
SerhiiFrolikov <116305785+SerhiiFrolikov@users.noreply.github.com>
2023-04-18 00:45:09 +0300

    task 3 done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,43 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const form = document.querySelector(".form");
+
+form.addEventListener("click", onCreatePromise);
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+function onCreatePromise(evt) {
+  evt.preventDefault();
+  const { delay, step, amount } = evt.currentTarget.elements;
+  let fieldDelay = Number(delay.value);
+  let fieldStep = Number(step.value);
+  let fieldAmount = Number(amount.value);
+
+  for (let i = 1; i <= fieldAmount; i += 1) {
+    fieldDelay += fieldStep;
+
+    createPromise(i, fieldDelay)
+      .then(({ position, delay }) => {
+        Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`,
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`,
+        );
+      });
+    evt.currentTarget.reset();
   }
 }
