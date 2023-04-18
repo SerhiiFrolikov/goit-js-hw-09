df4bdc65be8e85cf186fe45f3674ab401b731f49
SerhiiFrolikov <116305785+SerhiiFrolikov@users.noreply.github.com>
2023-04-18 17:16:04 +0000

    Deploying to gh-pages from @ SerhiiFrolikov/goit-js-hw-09@d7869f5054d6c831c93fc4b0293fa133736503c6 🚀

--- 01-color-switcher.242a8b1e.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("body"),e=document.querySelector("button[data-start]"),a=document.querySelector("button[data-stop]");let o=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`;e.addEventListener("click",(()=>{changeBgc=setInterval((()=>{e.disabled=!0,t.style.backgroundColor=o}),1e3)})),a.addEventListener("click",(()=>{clearInterval(changeBgc),e.disabled=!1}));
-//# sourceMappingURL=01-color-switcher.242a8b1e.js.map
