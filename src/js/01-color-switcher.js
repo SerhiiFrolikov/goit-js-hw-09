d7869f5054d6c831c93fc4b0293fa133736503c6
SerhiiFrolikov <116305785+SerhiiFrolikov@users.noreply.github.com>
2023-04-18 20:14:27 +0300

    second hot fix task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -2,11 +2,16 @@ const body = document.querySelector('body')
 const start = document.querySelector('button[data-start]');
 const stop = document.querySelector('button[data-stop]');
 
-let changeColor = getRandomHexColor();
+let changeBgc = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
 
 start.addEventListener("click", () => {
   changeBgc = setInterval(() => {
     start.disabled = true;
+    let changeColor = getRandomHexColor();
     body.style.backgroundColor = changeColor;
   }, 1000);
 });
@@ -15,7 +20,3 @@ stop.addEventListener("click", () => {
     clearInterval(changeBgc);
 start.disabled = false;
 });
-
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
-}
